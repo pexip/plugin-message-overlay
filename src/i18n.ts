3dec9cb046139f1d84a86847f519f6d5e735315c
Marcos Cereijo <marcos.rodriguez@pexip.com>
2025-08-04 12:13:26 +0200

    chore(i18n): move initI18n to its own function

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,13 +2,18 @@ import i18next from 'i18next'
 import Backend from 'i18next-http-backend'
 import { logger } from './logger'
 
-logger.info('Initializing i18n...')
+export const initI18n = async (): Promise<void> => {
+  logger.info('Initializing i18n...')
+  await i18next.use(Backend).init({
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
+    }
+  })
+}
 
-await i18next.use(Backend).init({
-  fallbackLng: 'en',
-  backend: {
-    loadPath: '/locales/{{lng}}/{{ns}}.json'
-  }
+initI18n().catch((error: unknown) => {
+  logger.error('Failed to initialize i18n:', error)
 })
 
 export { i18next }
