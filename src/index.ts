5e27ba243c863d8ac32e561fd1d311c5f9705eef
Marcos Cereijo <marcos.rodriguez@pexip.com>
2025-08-06 15:22:14 +0200

    chore: move the button from settings to the toolbar

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ import { i18next } from './i18n'
 
 const version = 1
 
+const icon =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(50, 50, 50)"><path d="M240-320h320v-80H240v80Zm400 0h80v-80h-80v80ZM240-480h80v-80h-80v80Zm160 0h320v-80H400v80ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm0 0v-480 480Z"/></svg>'
+
 const plugin = await registerPlugin({
   id: 'message-overlay',
   version
@@ -15,7 +18,7 @@ const plugin = await registerPlugin({
 
 setPlugin(plugin)
 
-let button: Button<'settingsMenu'> | null = null
+let button: Button<'toolbar'> | null = null
 let creatingButton = false
 
 const breakoutRooms = new Map<string, string>()
@@ -59,9 +62,14 @@ const addButton = async (): Promise<void> => {
   try {
     creatingButton = true
     button = await plugin.ui.addButton({
-      position: 'settingsMenu',
-      label: i18next.t('setMessageOverlay'),
-      inMeetingOnly: true,
+      position: 'toolbar',
+      tooltip: i18next.t('setMessageOverlay'),
+      icon: {
+        custom: {
+          main: icon,
+          hover: icon
+        }
+      },
       roles: ['chair']
     })
     button.onClick.add(handleClickButton)
